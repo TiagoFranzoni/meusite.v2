29c195e46e5152e6d91c8d37f3b00de343ddd1f3
Tiago Franzoni <tiago.franzoni@gmail.com>
2023-11-28 15:24:57 -0300

    Conferencia de login + Telas de login e de cadastro de usuários

--- bibliotecas_python/forms.py
+++ bibliotecas_python/forms.py
@@ -1,38 +1,11 @@
 """Docstring"""
-# from django.forms import Form
-# from django.forms import CharField 
-# from django.forms import EmailField 
-# from django.forms import PasswordInput
-# from django.contrib.auth.models import User
-#
-# class FormNovoUsuario(Form):
-#     """Docstring"""
-#     nome_de_usuario = CharField() 
-#     nome = CharField(required=False)
-#     email = EmailField(required=False) 
-#     senha = CharField(widget=PasswordInput)
-#     repeticao_senha = CharField(widget=PasswordInput)
-
-#     def save(self):
-#         """Docstring"""
-#         params = {
-#             'username': self.cleaned_data['nome_de_usuario'], 
-#             'email': self.cleaned_data['email'],
-#             'password': self.cleaned_data['senha'],
-#         }
-#         if self.cleaned_data['nome']:
-#             params['first_name'] = self.cleaned_data['nome']
-#         User.objects.create_user(**params)
-
-#     def clean_repeticao_senha(self):
-#         """Docstring"""
-#         senha1 = self.cleaned_data['senha']
-#         senha2 = self.cleaned_data['repeticao_senha']
-        
-#         if senha1 != senha2:
-#             raise ValidationError("As senhas devem ser iguais.")
-#         return senha
-
+from django.forms import Form
+from django.forms import CharField
+from django.forms import EmailField
+from django.forms import PasswordInput
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.core.exceptions import ValidationError
 from django.forms import ModelForm
 from .models import Bibliotecas
 
@@ -42,3 +15,48 @@ class FormBiblioteca(ModelForm):
         """Docstring"""
         model = Bibliotecas
         fields = '__all__'
+
+
+
+class FormLogin(Form):
+    """Docstring"""
+    nome_de_usuario = CharField()
+    senha = CharField(widget=PasswordInput)
+
+    def autentica(self, request):
+        """Docstring"""
+        username = self.cleaned_data['nome_de_usuario']
+        password = self.cleaned_data['senha']
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            raise ValidationError("Dados de login inválidos.")
+        login(request, user)
+
+
+class FormNovoUsuario(Form):
+    """Docstring"""
+    nome_de_usuario = CharField()
+    nome = CharField(required=False)
+    email = EmailField(required=False)
+    senha = CharField(widget=PasswordInput)
+    repeticao_senha = CharField(widget=PasswordInput)
+
+    def save(self):
+        """Docstring"""
+        params = {
+            'username': self.cleaned_data['nome_de_usuario'], 
+            'email': self.cleaned_data['email'],
+            'password': self.cleaned_data['senha'],
+        }
+        if self.cleaned_data['nome']:
+            params['first_name'] = self.cleaned_data['nome']
+        User.objects.create_user(**params)
+
+    def clean_repeticao_senha(self):
+        """Docstring"""
+        senha1 = self.cleaned_data['senha']
+        senha2 = self.cleaned_data['repeticao_senha']
+
+        if senha1 != senha2:
+            raise ValidationError("As senhas devem ser iguais.")
+        return senha2
--- bibliotecas_python/views.py
+++ bibliotecas_python/views.py
@@ -4,14 +4,28 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import UpdateView
 from bibliotecas_python.models import Bibliotecas
 from django.views.generic import View
-from django.shortcuts import render
-from .forms import FormBiblioteca
 from django.shortcuts import render, redirect
+from .forms import FormBiblioteca, FormLogin, FormNovoUsuario
 from django.template import RequestContext
-from .forms import FormBiblioteca
 from django.contrib import messages
 from django.urls import reverse
 from .models import Bibliotecas
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import authenticate, login
+from django.utils.decorators import method_decorator
+
+
+@method_decorator(login_required(login_url='/login/'), name='dispatch')
+class Home(View):
+    """docstring"""
+    template_name = 'bibliotecas/home.html'
+    context = {}
+
+    def get(self, request, *args, **kwargs):
+        """docstring"""
+        # self.context['counter'] = Bibliotecas.objects.filter(nome=True).count()
+        return render(request, 'bibliotecas/home.html', self.context)
 
 
 class BibliotecasView(ArchiveIndexView):
@@ -29,63 +43,29 @@ class BibliotecasDetail(DetailView):
     # pk_url_kwarg = 'biblioteca_id'
 
 
-class Home(View):
-    """docstring"""
-    template_name = 'bibliotecas/home.html'
-    context = {}
-
-    def get(self, request, *args, **kwargs):
-        """docstring"""
-        # self.context['counter'] = Bibliotecas.objects.filter(nome=True).count()
-        return render(request, 'bibliotecas/home.html', self.context)
-
-
-# class CriaUsuario(View):
-#     """docstring"""
-#     template_name = 'templates/cria_usuario.html'
-#     context = {}
-    
-#     def get(self, request, *args, **kwargs):
-#         """docstring"""
-#         self.context['form'] = FormNovoUsuario()
-#         return render_to_response(self.template_name, self.context, RequestContext(request))
-    
-#     def post(self, request, *args, **kwargs):
-#         """docstring"""
-#         form = FormNovoUsuario(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('/')
-#         else:
-#             self.context['form'] = form
-#             return render_to_response(self.template_name, self.context, RequestContext(request))
-
-
 class AdicionaBibliotecas(View):
     """docstring"""
     template_name = 'bibliotecas/cria_biblioteca.html'
     context = {}
-    
+
     def get(self, request, *args, **kwargs):
         """docstring"""
         self.context['form'] = FormBiblioteca()
         return render(request, self.template_name, self.context)
-        
+
     def post(self, request, *args, **kwargs):
         """docstring"""
         form = FormBiblioteca(request.POST)
         if form.is_valid():
-            nome_biblioteca = form.cleaned_data.get('nome')  # Substitua 'nome' pelo nome do campo correspondente
+            nome_biblioteca = form.cleaned_data.get('nome')
             if Bibliotecas.objects.filter(nome=nome_biblioteca).exists():
                 messages.error(request, f'Uma biblioteca com o nome "{nome_biblioteca}" já existe.')
                 self.context['form'] = form
                 return render(request, self.template_name, self.context)
-            else:
-                form.save()
-                return redirect('/bibliotecas/')
-        else:
-            self.context['form'] = form
-            return render(request, self.template_name, self.context)
+            form.save()
+            return redirect('/bibliotecas/')
+        self.context['form'] = form
+        return render(request, self.template_name, self.context)
 
 
 class EditaBibliotecas(UpdateView):
@@ -93,7 +73,6 @@ class EditaBibliotecas(UpdateView):
     model = Bibliotecas
     form_class = FormBiblioteca
     template_name = 'bibliotecas/edita_biblioteca.html'
-    # context_object_name = 'biblioteca'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -107,15 +86,83 @@ class EditaBibliotecas(UpdateView):
 class ExcluiBibliotecas(View):
     """docstring"""
     context = {}
-    
+
     def get(self, request, *args, **kwargs):
         """docstring"""
         self.context['form'] = FormBiblioteca()
         return render(request, self.template_name, self.context)
 
-    def post(self, request, *args, **kwargs):  # Mude 'delete' para 'post'
+    def post(self, request, *args, **kwargs):
         """docstring"""
         biblioteca = Bibliotecas.objects.get(pk=kwargs['pk'])
         biblioteca.delete()
         return redirect('/bibliotecas/')
 
+
+class Login(View):
+    """docstring"""
+    template_name = 'login_usuario.html'
+    context = {}
+
+    def get(self, request, *args, **kwargs):
+        """docstring"""
+        self.context['form'] = FormLogin()
+        return render(request, self.template_name, self.context)
+
+    def post(self, request, *args, **kwargs):
+        """docstring"""
+        form = FormLogin(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            self.context['form'] = form
+            self.context['error'] = 'Invalid username or password'
+            return render(request, self.template_name, self.context)
+
+    def clean(self):
+        """docstring"""
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username and password:
+            self.user_cache = authenticate(self.request, username=username, password=password)
+            if self.user_cache is None:
+                raise forms.ValidationError(
+                    self.error_messages['invalid_login'],
+                    code='invalid_login',
+                    params={'username': self.username_field.verbose_name},
+                )
+            else:
+                self.confirm_login_allowed(self.user_cache)
+
+        return self.cleaned_data
+
+
+class CriaUsuario(View):
+    """docstring"""
+    template_name = 'criar_usuario.html'
+    context = {}
+
+    def get(self, request, *args, **kwargs):
+        """docstring"""
+        self.context['form'] = FormNovoUsuario()
+        return render(request, self.template_name, self.context)
+
+
+    def post(self, request, *args, **kwargs):
+        """docstring"""
+        form = FormLogin(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            self.context['form'] = form
+            self.context['error'] = 'Username ou Senha inválidos'
+            return render(request, self.template_name, self.context)
